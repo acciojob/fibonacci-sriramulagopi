67a8f648b48838fc5d8bde4f7b61cf157c51d160
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2023-12-15 17:00:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
-// your code here
+	// your code here
+	let fib = 0;
+	let check = 0;
+	let a = 0;
+	let b = 1;
+	let c = 1;
+	while(num>1){
+		a=b;
+		b=c;
+		c=a+b;
+		fib=a;
+		num--;
+		check++;
+	}
+	return check>0 ? fib:0;
 }
 
-module.exports = fibonacci;
+console.log(fibonacci(6));
